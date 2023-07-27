e9594c7a0e9a0a915be871191ce307174f96695a
GabrielRenno <gabriel_renno@outlook.com>
2023-07-27 12:42:18 +0200

    c

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from Pdf_Bot.htmlTemplates import css, bot_template, user_template
+from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 from langchain.llms import OpenAI
 from langchain.chains.summarize import load_summarize_chain
